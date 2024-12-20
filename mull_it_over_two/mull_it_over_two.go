2a37bb884778451015b4b12716dba1e6f5ff41f3
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2024-12-21 00:28:14 +0530

    mull it over

---
+++ mull_it_over_two/mull_it_over_two.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("mull_it_over_two.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var final_ans int = 0
+	currently_enabled := true
+	for scanner.Scan() {
+		line := scanner.Text()
+		for i := 0; i < len(line)-5; i++ {
+			if line[i:i+2] == "do" {
+				currently_enabled = true
+			}
+			if line[i:i+5] == "don't" {
+				currently_enabled = false
+			}
+			if !currently_enabled {
+				continue
+			}
+			if line[i:i+3] == "mul" && line[i+3] == '(' {
+				numbers_string := ""
+				found_end := false
+				for j := i + 4; j < len(line) && j < i+15; j++ {
+					if line[j] == ')' {
+						found_end = true
+						break
+					}
+					numbers_string += string(line[j])
+				}
+				fmt.Println(numbers_string, found_end)
+				if !found_end {
+					continue
+				}
+				fmt.Println(numbers_string)
+				numbers := strings.Split(numbers_string, ",")
+				fmt.Println(numbers)
+				if len(numbers) != 2 {
+					continue
+				}
+				numbers_1, err := strconv.Atoi(numbers[0])
+				if err != nil {
+					continue
+				}
+				numbers_2, err := strconv.Atoi(numbers[1])
+				if err != nil {
+					continue
+				}
+				final_ans += numbers_1 * numbers_2
+			}
+		}
+	}
+	fmt.Print(final_ans)
+}
