377f3c87db3cfbd13b13ba1a5dfb0529c515610f
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2025-01-09 20:08:39 +0530

    found the christmas tree

--- restroom_redoubt_two/restroom_redoubt_two.go
+++ restroom_redoubt_two/restroom_redoubt_two.go
@@ -23,7 +23,7 @@ func main() {
 		fmt.Sscanf(line, "p=%d,%d v=%d,%d", &ax, &ay, &vx, &vy)
 		robots = append(robots, []int{ax, ay, vx, vy})
 	}
-	for num_moves := 10; num_moves <= 5000; num_moves++ {
+	for num_moves := 1000; num_moves <= 10000; num_moves++ {
 		grid := [mx][my]string{}
 		for i := 0; i < mx; i++ {
 			for j := 0; j < my; j++ {
@@ -42,13 +42,21 @@ func main() {
 		cnt := 0
 		for i := 0; i < mx; i++ {
 			for j := 0; j < my; j++ {
-				if grid[i][j] == grid[i][my-1-j] || grid[mx-1-i][j] == grid[i][j] {
+				if grid[i][j] == "x" && i < mx-1 && grid[i+1][j] == "x" {
+					cnt += 1
+				}
+				if grid[i][j] == "x" && i > 0 && grid[i-1][j] == "x" {
+					cnt += 1
+				}
+				if grid[i][j] == "x" && j < my-1 && grid[i][j+1] == "x" {
+					cnt += 1
+				}
+				if grid[i][j] == "x" && i > 0 && grid[i-1][j] == "x" {
 					cnt += 1
 				}
 			}
 		}
-
-		if cnt > 2000 {
+		if cnt > 300 {
 			fmt.Println(num_moves)
 			for i := 0; i < mx; i++ {
 				for j := 0; j < my; j++ {
